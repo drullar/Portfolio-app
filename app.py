9ecc885c9ed3a7af4ec7a976c2751a2d52a74100
antonio.busuladzich <>
2022-06-02 01:44:44 +0300

    updated path and port on which to run flask

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__,  template_folder='template', static_folder='static')
 @app.route('/')
 def index():
     return render_template("index.html")
-
+    
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8090, debug=True)
     
     
