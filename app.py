d4d6b30a0eab5ee8cc0d81a592e33eb526fb597c
antonio.busuladzich <>
2022-06-05 00:39:34 +0300

    Added some new code

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
+import os
 from flask import Flask, render_template
 app = Flask(__name__,  template_folder='template', static_folder='static')
 
-
 @app.route('/')
 def index():
-    return render_template("index.html")
-    
+    images = os.listdir('static/gallery-images')
+    images = ['gallery-images/' + file for file in images]
+    return render_template("index.html", hists=images)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8090, debug=True)
-    
+
     
---
+++ bin/start_server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Conda activate portfolio
+# python3 app.py
