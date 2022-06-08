9b71282673b2019957f9883dffbf8455ae7e9145
antonio.busuladzich <>
2022-06-08 20:00:05 +0300

    added dependancies, minor typo fix and bash update

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-#wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.12.0-Linux-x86_64.sh
-#./Miniconda3-py39_4.12.0-Linux-x86_64.sh
+wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.12.0-Linux-x86_64.sh
+chmod a+x /Miniconda3-py39_4.12.0-Linux-x86_64.sh
+
+./Miniconda3-py39_4.12.0-Linux-x86_64.sh
 #linuxyes
 #...
